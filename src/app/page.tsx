b0f60588f39c3b5d7add0225668d5ad1e8c9c54d
souhaib benbouzid <souhaib.benbouzid@gmail.com>
2025-07-25 15:33:07 +0100

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <p className="font-[family-name:var(--font-geist-mono)]">
-          Hi, I&apos;m Amani Benbouzid
-        </p>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center"></footer>
-    </div>
+    <main>
+      <h1>Welcome to Amani Ben&apos;s Portfolio</h1>
+      <p>This is a showcase of my work and projects.</p>
+    </main>
   );
 }
